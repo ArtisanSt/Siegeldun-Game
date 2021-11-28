af89101bd6ead8c51f31b70011f97e44fd764c0d
Lex <johnlexterpanti@gmail.com>
2021-11-28 22:18:18 +0800

    Status Bar: Stamina Bar Fixed
    Stamina Mechanics Dynamically Fixed
    Varying Attack Speed and its Animation

--- Siegeldun Game/Assets/Scripts/Player.cs
+++ Siegeldun Game/Assets/Scripts/Player.cs
@@ -8,13 +8,26 @@ public class Player : MonoBehaviour
     // ========================================= Game Properties =========================================
     public static int difficulty = 1; // Pseudo Difficulty
     private int idxDiff = difficulty - 1;
+    private float animationSpeed = 1f;
 
 
     // ========================================= Entity Properties =========================================
     private string entityName = "player";
 
+    // Property Scaling
     private float[] maxHpScaling = new float[] { 100f, 200f, 400f };
     private float[] healthRegenScaling = new float[] { .01f, .005f, .001f };
+    private float[] weaponStaminaCost = new float[] { 2f, 5f, 10f }; // Pseudo Stamina cost
+    private float[] weaponAttackSpeed = new float[] { 1f, 2f, 5f }; // Pseudo Attack Speed
+
+    // Battle Mechanics
+    public int entityWeapon = 0; // Pseudoweapon
+    public float entityDamage;
+    public float attackSpeed;
+    public float attackRange = 0.3f;
+    [SerializeField] Transform attackPoint;
+    [SerializeField] LayerMask enemyLayers;
+    [SerializeField] int attackDamage = 10;
 
     // HP Mechanics
     private float entityHp;
@@ -22,9 +35,12 @@ public class Player : MonoBehaviour
     private float regenDelay = 3f;
     private float maxHealth;
 
-    // Battle Mechanics
-    public float entityDamage;
-    public float attackSpeed;
+    // Stamina Mechanics
+    private float entityStam;
+    private float stamRegen;
+    private float maxStam;
+    private bool attacking;
+    private float EqWeaponStamCost;
 
 
     // ========================================= Status Bar Properties =========================================
@@ -36,45 +52,127 @@ public class Player : MonoBehaviour
     [SerializeField] Text statHpText;
 
     // HP Parameter Declaration
-    private bool isDamaged = false;
-    private bool regenerate = false;
-
-    private float tick;
+    private float hpTick;
     private float hpRegenTimer;
 
+    // Stamina Component Declaration
+    [SerializeField] Image StamBar;
+
+    private int statStam;
+    [SerializeField] Text statStamText;
+
     // Called when Start
-    public void HpInitialization()
+    public void StatusInitialization()
     {
+        // HP Initialization
         maxHealth = maxHpScaling[idxDiff];
         healthRegen = healthRegenScaling[idxDiff];
         entityHp = maxHealth;
         hpRegenTimer = 0f;
+
+        // Stamina Initialization
+        maxStam = 100f;
+        stamRegen = .01f;
+        entityStam = maxStam;
+        EqWeaponStamCost = weaponStaminaCost[entityWeapon];
+        attacking = false;
+
+        // Battle Initialization
+        attackSpeed = 1f;
     }
 
-    // Update is called once per frame
-    public void HpUpdate()
+
+    // ========================================= Unity Properties =========================================
+    // Component Declaration
+    private Rigidbody2D body;
+    private CapsuleCollider2D capColl;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    // Movement Parameters
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    [SerializeField] private LayerMask groundLayers;
+    private float dirX;
+    private enum MovementAnim { idle, run, jump, fall };
+    private MovementAnim state;
+
+
+    // ========================================= UNITY MAIN METHODS =========================================
+    // Initializes when the Player Script is called
+    void Start()
     {
-        // Player UI HP Frame Update
+        // Component Variable Definition
+        body = GetComponent<Rigidbody2D>();
+        capColl = GetComponent<CapsuleCollider2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        StatusInitialization();
+    }
+
+    // Updates Every Frame
+    void Update()
+    {
+        // Movement Animation
+        dirX = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(dirX * speed, body.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && capColl.IsTouchingLayers(groundLayers))
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+        }
+
+        // Attack Code
+        attacking = GetComponent<SpriteRenderer>().sprite.ToString().Substring(0, 11) == "Noob_Attack"; // Anti-spamming code
+        if (Input.GetKeyDown(KeyCode.Mouse0) && attacking == false && EqWeaponStamCost <= entityStam)
+        {
+            Attack();
+        }
+
+        // Pseudo Attack Speed Changer
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            attackSpeed += 0.1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            attackSpeed -= 0.1f;
+        }
+
+        // Pseudo Damage Taken 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TakeDamage(Random.Range(5, 10));
+        }
+
+        // Pseudo Heal
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            HpRegen(20f, "instant");
+        }
+
+
+        AnimationState();
         PlayerHealthBarUpdate();
+        PlayerStaminaBarUpdate();
     }
 
-    // Healthbar UI Update
-    public void PlayerHealthBarUpdate()
+
+    // ========================================= STATUS BAR METHODS =========================================
+    // Health Bar UI Update
+    private void PlayerHealthBarUpdate()
     {
-        // Natural Healing starter
+        // Natural HP Heal starter
         if (entityHp < maxHealth)
         {
             hpRegenTimer += Time.deltaTime;
 
             if (hpRegenTimer >= regenDelay)
             {
-                Regen(healthRegen);
-                regenerate = true;
+                HpRegen(healthRegen);
             }
-        }
-        else
-        {
-            regenerate = false;
         }
 
         // HP UI Parameters
@@ -103,21 +201,21 @@ public class Player : MonoBehaviour
 
             if (fillB > fillF) // When Back Health Bar is greater than Front Health Bar
             {
-                tick += 0.1f;
+                hpTick += 0.1f;
                 HealthbarB.color = Color.red;
-                percentChangeB = (-0.001f / netRegenB) * tick;
+                percentChangeB = (-0.001f / netRegenB) * hpTick;
                 HealthbarB.fillAmount += Mathf.Lerp(0, netRegenB, percentChangeB);
             }
             else if (fillB < fillF)// When Back Health Bar is lower than Front Health Bar
             {
-                tick = 0f;
+                hpTick = 0f;
                 percentChangeB = healthRegen / (maxHealth * netRegenB);
                 HealthbarB.fillAmount = fillF;
             }
         }
         else
         {
-            tick = 0f;
+            hpTick = 0f;
             HealthbarF.color = Color.cyan;
         }
 
@@ -126,94 +224,68 @@ public class Player : MonoBehaviour
         statHpText.text = statHp.ToString() + " / " + maxHealth.ToString();
     }
 
-    public void TakeDamage(float damageTaken)
-    {
-        regenerate = false;
-        isDamaged = true;
-        entityHp -= damageTaken;
-        hpRegenTimer = 0f;
-    }
-
-    public void Regen(float healAmount, string HealSpeed = "overtime")
+    public void HpRegen(float healAmount, string HealSpeed = "overtime")
     {
         entityHp += healAmount;
-        if(HealSpeed == "instant")
+        if (HealSpeed == "instant")
         {
             HealthbarF.fillAmount = entityHp / maxHealth;
         }
         entityHp = Mathf.Clamp(entityHp, 0, maxHealth);
     }
 
-
-    // ========================================= Unity Properties =========================================
-    // Component Declaration
-    private Rigidbody2D body;
-    private CapsuleCollider2D capColl;
-    private Animator animator;
-    private SpriteRenderer sprite;
-    private float dirX;
-
-    // Variable Serialization
-    [SerializeField] float speed;
-    [SerializeField] float jumpForce;
-    [SerializeField] private LayerMask groundLayers;
-
-    // Attack Parameters
-    public bool attacking = false;
-    public float attackRange = 0.3f;
-    [SerializeField] Transform attackPoint; 
-    [SerializeField] LayerMask enemyLayers;
-    [SerializeField] int attackDamage = 10;
-
-    private enum MovementAnim { idle, run, jump, fall };
-    private MovementAnim state;
-
-    // Initializes when the Player Script is called
-    private void Start()
+    // Status Bar UI Update
+    private void PlayerStaminaBarUpdate()
     {
-        // Component Variable Definition
-        body = GetComponent<Rigidbody2D>();
-        capColl = GetComponent<CapsuleCollider2D>();
-        animator = GetComponent<Animator>();
-        sprite = GetComponent<SpriteRenderer>();
+        // Natural Stamina Heal starter
+        if (entityStam < maxStam)
+        {
+            StamRegen(stamRegen);
+        }
 
-        HpInitialization();
+        // Stamina UI Parameters
+        float fillS = StamBar.fillAmount;
+        float stamFraction = entityStam / maxStam;
+
+        // Stamina Bar UI Updater
+        if (fillS != stamFraction)
+        {
+            if (fillS > stamFraction)
+            {
+                StamBar.fillAmount = stamFraction;
+            }
+            else if (fillS < stamFraction)
+            {
+                float netRegenS = stamFraction - fillS;
+                float percentChangeS = stamRegen / (maxStam * netRegenS);
+                StamBar.fillAmount += Mathf.Lerp(0, netRegenS, percentChangeS);
+            }
+        }
+
+        // Stamina UI Text
+        statStam = (int)entityStam;
+        statStamText.text = statStam.ToString() + " / " + maxStam.ToString();
     }
 
-    // Updates Every Frame
-    private void Update()
+    public void StamRegen(float StamAmount, string HealSpeed = "overtime")
     {
-        dirX = Input.GetAxisRaw("Horizontal");
-        body.velocity = new Vector2(dirX * speed, body.velocity.y);
-
-        if (Input.GetButtonDown("Jump") && capColl.IsTouchingLayers(groundLayers))
+        Debug.Log(true);
+        entityStam += StamAmount;
+        if (HealSpeed == "instant")
         {
-            body.velocity = new Vector2(body.velocity.x, jumpForce);
+            StamBar.fillAmount = entityStam / maxStam;
         }
-
-        AnimationState();
-
-
-        // Attack Code
-        attacking = GetComponent<SpriteRenderer>().sprite.ToString().Substring(0, 11) == "Noob_Attack"; // Anti-spamming code
-        if (Input.GetKeyDown(KeyCode.Mouse0) && attacking == false)
-        {
-            Attack();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            TakeDamage(Random.Range(5, 10));
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Regen(20f, "instant");
-        }
-
-        HpUpdate();
+        entityStam = Mathf.Clamp(entityStam, 0, maxStam);
     }
 
+    public void TakeDamage(float damageTaken)
+    {
+        entityHp -= damageTaken;
+        hpRegenTimer = 0f;
+    }
+
+
+    // ========================================= ANIMATION METHODS =========================================
     private void AnimationState()
     {
         if (dirX == 0)
@@ -239,19 +311,29 @@ public class Player : MonoBehaviour
         }
 
         animator.SetInteger("state", (int)state);
+
+        if (attacking)
+        {
+            animator.speed = attackSpeed;
+        }
+        else
+        {
+            animator.speed = animationSpeed;
+        }
     }
 
     private void Attack()
     {
         attacking = true;
-        // Play Attack anim
+        entityStam -= EqWeaponStamCost;
+        // Play Attack animation
         animator.SetTrigger("attack");
         // Detect enemy in range
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
         attacking = false;
 
         // Damage
-        foreach(Collider2D enemy in hitEnemies)
+        foreach (Collider2D enemy in hitEnemies)
         {
             enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
         }
@@ -264,7 +346,7 @@ public class Player : MonoBehaviour
 
     void OnDrawGizmosSelected()
     {
-        if(attackPoint == null)
+        if (attackPoint == null)
             return;
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
