4d0bcd5e4935fcb7ec93d687509488f10a92624b
Lex <johnlexterpanti@gmail.com>
2021-11-28 20:13:13 +0800

    Status Bar: Health Bar Fixed
    Health Mechanics Dynamically Fixed

--- Siegeldun Game/Assets/Scripts/GameProperties.cs
+++ Siegeldun Game/Assets/Scripts/GameProperties.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCombat : MonoBehaviour
+public class GameProperties : MonoBehaviour
 {
+    public int difficulty;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.difficulty = 1;
     }
 
     // Update is called once per frame
--- Siegeldun Game/Assets/Scripts/Player.cs
+++ Siegeldun Game/Assets/Scripts/Player.cs
@@ -1,15 +1,159 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    // ========================================= Game Properties =========================================
+    public static int difficulty = 1; // Pseudo Difficulty
+    private int idxDiff = difficulty - 1;
+
+
+    // ========================================= Entity Properties =========================================
+    private string entityName = "player";
+
+    private float[] maxHpScaling = new float[] { 100f, 200f, 400f };
+    private float[] healthRegenScaling = new float[] { .01f, .005f, .001f };
+
+    // HP Mechanics
+    private float entityHp;
+    private float healthRegen;
+    private float regenDelay = 3f;
+    private float maxHealth;
+
+    // Battle Mechanics
+    public float entityDamage;
+    public float attackSpeed;
+
+
+    // ========================================= Status Bar Properties =========================================
+    // HP Component Declaration
+    [SerializeField] Image HealthbarF;
+    [SerializeField] Image HealthbarB;
+
+    private int statHp;
+    [SerializeField] Text statHpText;
+
+    // HP Parameter Declaration
+    private bool isDamaged = false;
+    private bool regenerate = false;
+
+    private float tick;
+    private float hpRegenTimer;
+
+    // Called when Start
+    public void HpInitialization()
+    {
+        maxHealth = maxHpScaling[idxDiff];
+        healthRegen = healthRegenScaling[idxDiff];
+        entityHp = maxHealth;
+        hpRegenTimer = 0f;
+    }
+
+    // Update is called once per frame
+    public void HpUpdate()
+    {
+        // Player UI HP Frame Update
+        PlayerHealthBarUpdate();
+    }
+
+    // Healthbar UI Update
+    public void PlayerHealthBarUpdate()
+    {
+        // Natural Healing starter
+        if (entityHp < maxHealth)
+        {
+            hpRegenTimer += Time.deltaTime;
+
+            if (hpRegenTimer >= regenDelay)
+            {
+                Regen(healthRegen);
+                regenerate = true;
+            }
+        }
+        else
+        {
+            regenerate = false;
+        }
+
+        // HP UI Parameters
+        float fillF = HealthbarF.fillAmount;
+        float fillB = HealthbarB.fillAmount;
+        float hpFraction = entityHp / maxHealth;
+
+        // Health Bar UI Updater
+        if (fillB != fillF || fillF != hpFraction || fillB != hpFraction)
+        {
+            float netRegenF = hpFraction - fillF;
+            float netRegenB = fillF - fillB;
+            float percentChangeB, percentChangeF;
+
+            if (fillF > hpFraction) // When Front Health Bar is greater than the real health
+            {
+                HealthbarF.color = Color.cyan;
+                HealthbarF.fillAmount = hpFraction;
+            }
+            else if (fillF < hpFraction) // When Front Health Bar is lower than the real health
+            {
+                HealthbarF.color = Color.green;
+                percentChangeF = healthRegen / (maxHealth * netRegenF);
+                HealthbarF.fillAmount += Mathf.Lerp(0, netRegenF, percentChangeF);
+            }
+
+            if (fillB > fillF) // When Back Health Bar is greater than Front Health Bar
+            {
+                tick += 0.1f;
+                HealthbarB.color = Color.red;
+                percentChangeB = (-0.001f / netRegenB) * tick;
+                HealthbarB.fillAmount += Mathf.Lerp(0, netRegenB, percentChangeB);
+            }
+            else if (fillB < fillF)// When Back Health Bar is lower than Front Health Bar
+            {
+                tick = 0f;
+                percentChangeB = healthRegen / (maxHealth * netRegenB);
+                HealthbarB.fillAmount = fillF;
+            }
+        }
+        else
+        {
+            tick = 0f;
+            HealthbarF.color = Color.cyan;
+        }
+
+        // HP UI Text
+        statHp = (int)entityHp;
+        statHpText.text = statHp.ToString() + " / " + maxHealth.ToString();
+    }
+
+    public void TakeDamage(float damageTaken)
+    {
+        regenerate = false;
+        isDamaged = true;
+        entityHp -= damageTaken;
+        hpRegenTimer = 0f;
+    }
+
+    public void Regen(float healAmount, string HealSpeed = "overtime")
+    {
+        entityHp += healAmount;
+        if(HealSpeed == "instant")
+        {
+            HealthbarF.fillAmount = entityHp / maxHealth;
+        }
+        entityHp = Mathf.Clamp(entityHp, 0, maxHealth);
+    }
+
+
+    // ========================================= Unity Properties =========================================
+    // Component Declaration
     private Rigidbody2D body;
-    private CapsuleCollider2D collider;
+    private CapsuleCollider2D capColl;
     private Animator animator;
-    public   SpriteRenderer sprite;
+    private SpriteRenderer sprite;
     private float dirX;
-    
+
+    // Variable Serialization
     [SerializeField] float speed;
     [SerializeField] float jumpForce;
     [SerializeField] private LayerMask groundLayers;
@@ -24,20 +168,25 @@ public class Player : MonoBehaviour
     private enum MovementAnim { idle, run, jump, fall };
     private MovementAnim state;
 
+    // Initializes when the Player Script is called
     private void Start()
     {
+        // Component Variable Definition
         body = GetComponent<Rigidbody2D>();
-        collider = GetComponent<CapsuleCollider2D>();
+        capColl = GetComponent<CapsuleCollider2D>();
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+
+        HpInitialization();
     }
 
+    // Updates Every Frame
     private void Update()
     {
         dirX = Input.GetAxisRaw("Horizontal");
         body.velocity = new Vector2(dirX * speed, body.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && collider.IsTouchingLayers(groundLayers))
+        if (Input.GetButtonDown("Jump") && capColl.IsTouchingLayers(groundLayers))
         {
             body.velocity = new Vector2(body.velocity.x, jumpForce);
         }
@@ -51,6 +200,18 @@ public class Player : MonoBehaviour
         {
             Attack();
         }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TakeDamage(Random.Range(5, 10));
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Regen(20f, "instant");
+        }
+
+        HpUpdate();
     }
 
     private void AnimationState()
@@ -94,6 +255,11 @@ public class Player : MonoBehaviour
         {
             enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
         }
+    }
+
+    private void Consume()
+    {
+
     }
 
     void OnDrawGizmosSelected()
