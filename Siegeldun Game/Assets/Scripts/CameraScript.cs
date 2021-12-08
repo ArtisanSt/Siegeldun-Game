785a04db60e5a6213e4f77188ef33499b23c3c2d
jamescorpuz88 <jamescorpuz88@gmail.com>
2021-12-08 18:07:00 +0800

    dev v0.6.3j
    Camera Changes
    Added Cinemachine
    CameraScript Tweak

--- Siegeldun Game/Assets/Scripts/CameraScript.cs
+++ Siegeldun Game/Assets/Scripts/CameraScript.cs
@@ -5,9 +5,37 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     [SerializeField] Transform player;
-    [SerializeField] float cameraYPos = 0;
- 
-     void Update () {
-         transform.position = new Vector3 (player.position.x, player.position.y + cameraYPos, player.position.z - 10);
-     }
+    [SerializeField] Vector3 cameraOffset;
+    [SerializeField] bool yFreeze;
+
+    [Range(1,10)]
+    [SerializeField] float smoothFactor = 2.5f;
+
+    public void Start()
+    {
+        cameraOffset.z = -10;
+    }
+
+    public void FixedUpdate()
+    {
+        Follow();
+    }
+    
+    private void Follow()
+    {
+        Vector3 playerPosition;
+
+        if(yFreeze)
+        {
+            cameraOffset.y = -1;
+            playerPosition = new Vector3 (player.position.x + cameraOffset.x, cameraOffset.y, cameraOffset.z);
+        }
+        else
+        {
+            cameraOffset.y = 2;
+            playerPosition = player.position + cameraOffset;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, playerPosition, smoothFactor*Time.fixedDeltaTime); // Linear Interpolation, Moves in an axis in a linear motion
+    }
 }
