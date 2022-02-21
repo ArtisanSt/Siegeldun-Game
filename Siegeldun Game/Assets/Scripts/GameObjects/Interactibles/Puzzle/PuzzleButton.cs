789ff0da761d0cdeffd37e87496d8203b6509d20
jamescorpuz88 <jamescorpuz88@gmail.com>
2022-02-21 16:58:22 +0800

    Level_3 merge 1

---
+++ Siegeldun Game/Assets/Scripts/GameObjects/Interactibles/Puzzle/PuzzleButton.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleButton : Structures
+{
+    [SerializeField] List<GameObject> torches;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    // Interaction Event
+    public override void Interact()
+    {
+        if (!isSelected) return;
+
+        transform.GetComponent<Animator>().SetTrigger("Press");
+
+        foreach(GameObject torch in torches)
+        {
+            torch.GetComponent<TorchScript>().switchTorch();
+        }
+
+        if(!PuzzleManager.Instance.isAllOn())
+            PuzzleManager.Instance.CloseGates();
+        else
+            PuzzleManager.Instance.OpenGates();
+        
+    }
+
+    public void DisableInteract()
+    {
+        isInteractible = false;
+    }
+}
